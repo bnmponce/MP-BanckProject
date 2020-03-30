0a7bf3269fc595627ef9ec9dfcea25420058da62
MagdalenaPonce <magdalena.ponce@jalasoft.com>
2020-03-30 19:38:36 -0400

    The withdrow method is implement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
         // Welcome to the BankApp
-        Menus menu = new Menus("Jerry", "BO10006789");
-        menu.printWelcome();
-        // Main Menu
-        menu.printMenu();
-        // Press a key to select an option
-        menu.selectOption();
+        Menus menu = new Menus();
+        menu.printWelcome("Jerry", "BO10006789");
+
 
     }
 
